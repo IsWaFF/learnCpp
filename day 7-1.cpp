b31fab3c33f73812a93329fc5dd3e555b07515ad
IsWaFF <kosodima228@gmail.com>
2026-03-25 23:06:30 +0300

    day 7

---
+++ day 7-1.cpp
@@ -0,0 +1,32 @@
+// day 5-1 masives
+#include <iostream>
+#include <time.h>
+
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    long pos, el;
+
+    
+    cout << "позиций в массиве:";
+    cin >> pos;
+    while (pos <= 0) {
+        cout << "iq? reenter(>0):";
+        cin >> pos;
+    }
+    long *mas = new long[pos];
+    for (long i = 0; i < pos; i++) {
+        
+        cout << i << " элемент = ";
+        cin >> el;
+        mas[i] = el;
+    }
+    for (long i = 0; i < pos; i++) {
+        
+        cout << i << " элемент - " << mas[i] << endl;
+    }
+
+    return 0;
+}
