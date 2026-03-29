d53d08e8e37cbc9586ef36c9680b1c071f259da4
IsWaFF <kosodima228@gmail.com>
2026-03-29 17:13:08 +0300

    10-1

---
+++ day 10-1.cpp
@@ -0,0 +1,29 @@
+// day 10-1
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    /*ofstream file("test.txt", ios_base::out);
+    if (file.is_open()) {
+        file << "Wassup bitch"; // создание
+        file.close();
+    }*/
+
+    ifstream file("test.txt");
+    if (file.is_open()) {
+        string temp;
+        file >> temp;
+        file.close();
+        cout << temp << endl;
+    }
+
+
+    return 0;
+}
+
